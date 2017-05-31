2f2643d1ca6e84e3efd5ae9c2b19d3b1496a85fd
wenzouquan <359945126@qq.com>
2017-05-31 09:25:42 +0800

    12

---
+++ src/dict.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'phpkitDb' => array('empty' => '请配置您的数据库', 'placeholder' => 'array(
+		"host" => "localhost",
+		"username" => "root",
+		"password" => "",
+		"dbname" => "phpkit",
+		"charset" => "utf8",
+	)', ),
+
+);
