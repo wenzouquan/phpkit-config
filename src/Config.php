444042f0b02bb10c877a16ced472b0a287ae11ee
wenzouquan <359945126@qq.com>
2017-08-16 13:33:00 +0800

    config保存为php文件

--- src/Config.php
+++ src/Config.php
@@ -4,12 +4,19 @@ use Phalcon\Mvc\View\Simple as SimpleView;
 
 class Config {
 	function __construct($param = array()) {
+
+		$param['configDir'] = $param['configDir'] ? $param['configDir'] : dirname(__FILE__) . '/data/';
+		if (!$param['configDir']) {
+			\phpkit\helper\mk_dir($cacheDir);
+		}
+		$this->params = $param;
+
 		$frontCache = new \Phalcon\Cache\Frontend\Data(array(
 			"lifetime" => 172800000,
 		));
 
 		$this->cache = new \Phalcon\Cache\Backend\File($frontCache, array(
-			"cacheDir" => dirname(__FILE__) . '/data/',
+			"cacheDir" => $cacheDir,
 		));
 		//模板渲染
 		$this->view = new SimpleView();
@@ -40,19 +47,36 @@ class Config {
 				exit();
 			}
 		}
-		$value = $this->cache->get($name);
+		$value = require_once $this->params['configDir'] . $name . ".php";
 		return $value;
 	}
 
 	function save($name, $value) {
-		return $this->cache->save($name, $value);
+		$str_tmp = "<?php\r\n"; //得到php的起始符。$str_tmp将累加
+		$str_tmp .= "return ";
+		if (is_array($value)) {
+			$str_tmp .= \phpkit\helper\arrayeval($value);
+		} else if (is_string($value)) {
+			$str_tmp .= "'" . $value . "'";
+		} else {
+			$str_tmp .= (string) $value;
+		}
+
+		$str_tmp = str_replace("Array", "array", $str_tmp);
+		$str_tmp .= ";";
+		$ret = \phpkit\helper\saveFile($this->params['configDir'] . $name . ".php", $str_tmp);
+		if (!$ret) {
+			throw new \Exception("配置生成失败！", 1);
+
+		}
+		//return $this->cache->save($name, $value);
 	}
 
 	function exists($name) {
-		return $this->cache->exists($name);
+		return is_file($this->params['configDir'] . $name . ".php");
 	}
 
 	function delete($name) {
-		$this->cache->delete($name);
+		return unlink($this->params['configDir'] . $name . ".php");
 	}
 }
